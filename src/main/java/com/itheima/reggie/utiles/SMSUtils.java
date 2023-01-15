65fb0c8c10c08f4a953f3066d6732865d7e7a8f6
liujun <739814336@qq.com>
2023-01-15 15:20:30 +0800

    Day5-新增套餐-短信发送-修改

--- src/main/java/com/itheima/reggie/utiles/SMSUtils.java
+++ src/main/java/com/itheima/reggie/utiles/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tSdSfxLp76xYhh7UZEf", "fpa0qY7f9epyOPnlbtpVmgGJ12KpVp");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
