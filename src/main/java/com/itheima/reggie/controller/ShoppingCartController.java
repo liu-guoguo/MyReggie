a153de8a68fef8d0a4b7362759eff762869cf67d
liujun <739814336@qq.com>
2023-01-16 23:13:53 +0800

    Day6-购物车

---
+++ src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
@@ -0,0 +1,71 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.reggie.common.BaseContext;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.ShoppingCart;
+import com.itheima.reggie.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 购物车
+ */
+@RestController
+@RequestMapping("/shoppingCart")
+@Slf4j
+public class ShoppingCartController {
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    /**
+     * 添加购物车
+     * @param shoppingCart
+     * @return
+     */
+    @PostMapping("/add")
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart){
+        //这里需要在前端展示一个页面，所以返回的是一个实例对象
+        log.info("购物车数据：{}",shoppingCart);
+
+        //设置用户id，指定当前是哪个用户的购物车数据
+        Long userId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(userId);
+        //查询当前菜品或者套餐是否在购物车中
+        LambdaQueryWrapper<ShoppingCart> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        //根据userId和dishId来锁定菜品,分别设置查询条件
+        lambdaQueryWrapper.eq(ShoppingCart::getUserId,userId);
+        if(shoppingCart.getDishId() != null){
+            //添加到购物车的是菜品
+            lambdaQueryWrapper.eq(ShoppingCart::getDishId,shoppingCart.getDishId());
+
+        }else{
+            //添加到购物车的是套餐
+            lambdaQueryWrapper.eq(ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
+        }
+        //查询当前菜品或者套餐是否在购物车中
+        //SQL:select * from shopping_cart where user_id = ? and dish_id = ?
+        ShoppingCart one = shoppingCartService.getOne(lambdaQueryWrapper);
+
+        if(one != null){
+            //如果存在，则数量+1
+            Integer number = one.getNumber();
+            one.setNumber(number + 1);
+            shoppingCartService.updateById(one);
+        }else{
+            //不存在，则添加到购物车，并且默认设置为1
+            shoppingCart.setNumber(1);
+            shoppingCartService.save(shoppingCart);
+            one = shoppingCart;
+        }
+
+        return R.success(one);
+    }
+
+
+}
