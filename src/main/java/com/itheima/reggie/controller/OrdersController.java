f6308402b93bbf15cf850351411a449aaa9898b4
liujun <739814336@qq.com>
2023-01-29 21:49:30 +0800

    Day6-用户下单2

--- src/main/java/com/itheima/reggie/controller/OrdersController.java
+++ src/main/java/com/itheima/reggie/controller/OrdersController.java
@@ -2,7 +2,12 @@ package com.itheima.reggie.controller;
 
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Orders;
+import com.itheima.reggie.service.OrdersService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 public class OrdersController {
 
-    public R<Orders> submits(){
-        return null;
+    @Autowired
+    private OrdersService ordersService;
+
+    /**
+     * 用户下单
+     * @param orders
+     * @return
+     */
+    @PostMapping("/submit")
+    public R<String> submit(@RequestBody Orders orders){  //用的post方式，参数在请求体中，所以要加RequestBody
+        log.info("订单数据：{}",orders);
+        ordersService.submit(orders);
+        return R.success("下单成功");
     }
 }
--- src/main/java/com/itheima/reggie/service/OrdersService.java
+++ src/main/java/com/itheima/reggie/service/OrdersService.java
@@ -4,4 +4,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.itheima.reggie.entity.Orders;
 
 public interface OrdersService extends IService<Orders> {
+
+    /**
+     * 用户下单
+     * @param orders
+     */
+    public void submit(Orders orders);
 }
